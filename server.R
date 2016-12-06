c1268d94947b6bfed58f6c6df5b4d01d17a7a524
Leo Wang <leo42k@leos-mbp.sph.ad.jhsph.edu>
2016-12-06 16:33:45 -0500

    use term information

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 info <- read.csv(file.path("info.csv"), stringsAsFactors = FALSE)
-
+info[,3] == 
 
 library(shiny)
 library(DT)
@@ -8,9 +8,23 @@ shinyServer(function(input, output, session) {
     df = as.data.frame(info[,1:3])
     options(DT.options = list(pageLength = 5))
     
+    datasetInput <- reactive({
+        switch(input$term,
+               "1" = "1st",
+               "2" = "2st",
+               "3" = "3st",
+               "4" = "4st",
+               "S" = "Summer",
+               "S1" = "Summer Inst.",
+               "Int" = "Winter Inst")
+    })
+    
+    term = datasetInput()
+    
     # row selection
 
-    output$x13 = DT::renderDataTable(df)
+    df_use <- df[df[,3] == term,]
+    output$x13 = DT::renderDataTable(df_use)
     output$y13 = renderPrint(input$x13_rows_selected)
     
     output$x14 = DT::renderDataTable(df, selection = "none")
