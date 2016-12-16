388f63362476f57c71d1bd52d71147f6598cde85
Leo Wang <leo42k@Leos-MacBook-Pro.local>
2016-12-16 10:30:03 -0500

    update yang

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 info <- read.csv(file.path("info.csv"), stringsAsFactors = FALSE)
-source(file.path("Yang", "code.sent.R"))
+# source(file.path("Yang", "code.sent.R"))
 
 library(shiny)
 library(DT)
