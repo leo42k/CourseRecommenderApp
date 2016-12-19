3a5f4582d17ecef973c22f072b1a6e1d3cf5f710
Leo Wang <leo42k@Leos-MacBook-Pro.local>
2016-12-19 00:16:27 -0500

    rsconnect

---
+++ rsconnect.R
@@ -0,0 +1,6 @@
+install.packages("rsconnect")
+library(rsconnect)
+rsconnect::setAccountInfo(name='jhubiostatistics', token='A12E679D3BDB7BD8EBDBB070ECB1A2DC', secret='cxQUJL4Nf+I56VBOIHa3Nq5DE0zQwCqOL2ueBune')
+
+deployApp() 
+
